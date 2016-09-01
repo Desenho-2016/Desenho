f058820ee8c4d7eefb40e5a191153e2be71a2979
Sabryna <sabryna.sousa1323@gmail.com>
2016-09-01 08:05:24 -0300

    Fixing bugs to update and edit user

--- SiCAD/app/controllers/users_controller.rb
+++ SiCAD/app/controllers/users_controller.rb
@@ -24,6 +24,27 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
      end
 
+     def update
+         @user = User.find(params[:id])
+         if @user.update_attributes(user_params)
+             flash[:success]= "Usuário atualizado"
+             redirect_to @user
+         else
+             render 'edit'
+         end
+     end
+
+     def destroy
+         @user = User.find(params[:id])
+         @user.destroy
+         flash[:success] = "Usuário foi deletado"
+         redirect_to users_path
+     end
+
+     def edit
+         @user = User.find(params[:id])
+     end
+
     private
     def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
