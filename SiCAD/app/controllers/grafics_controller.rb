f164faf56e3a097e4872a6b29eba47a255c7b1aa
ruanHerculano <ruannawe95@gmail.com>
2016-09-30 00:54:26 -0300

    add grafics examples

---
+++ SiCAD/app/controllers/grafics_controller.rb
@@ -0,0 +1,4 @@
+class GraficsController < ApplicationController
+  def home
+  end
+end
--- SiCAD/config/routes.rb
+++ SiCAD/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'grafics/home'
+
   resources :mentions
   resources :disciplines
   resources :periods
