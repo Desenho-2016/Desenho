198d3dfb7e0d13e8066c1ccbc9283027258654f0
ruanHerculano <ruannawe95@gmail.com>
2016-09-30 00:02:46 -0300

    add collection_select

--- SiCAD/app/controllers/users_controller.rb
+++ SiCAD/app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     end
 
     def create
+      debugger
         @user = User.new(user_params)
         if @user.save
             SignupMailer.confirm_email(@user).deliver
